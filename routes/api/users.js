610a676a6f8f9ce1b131516469a4a5a30030189e
trizzi <patrickakhamiogu@gmail.com>
2022-03-18 16:33:07 +0100

    Added user schema, added models and added user validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const {
+  body,
+  validationResult,
+} = require('express-validator');
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    body('name', 'Name is required').not().isEmpty(),
+    body('email', 'Please enter a valid email').isEmail,
+    body(
+      'password',
+      'Please enter a password of at least 6 characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
