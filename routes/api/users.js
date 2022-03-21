81fa060b511cee5ddd692883088d514e04d782a0
trizzi <patrickakhamiogu@gmail.com>
2022-03-21 16:06:17 +0100

    Created auth middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,8 @@ const {
 } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // @route   POST api/users
 // @desc    Register User
@@ -60,7 +62,21 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
