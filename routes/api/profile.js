32c8a34d6783ee1e9f26b751b6e1117487380d46
trizzi <patrickakhamiogu@gmail.com>
2022-03-24 04:22:11 +0100

    added delete profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -176,4 +176,22 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   Delete api/profile/
+// @desc    Delete profile, User & Posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // todo - remove users posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove User
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
