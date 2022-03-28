1e0b5711b9a5fd8e52cfcea5dd83d3b908907d3c
trizzi <patrickakhamiogu@gmail.com>
2022-03-28 09:13:26 +0100

    Added get posts by Id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -41,11 +41,50 @@ router.post(
       const post = await newPost.save();
 
       res.json(post);
-    } catch (error) {
+    } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
     }
   }
 );
+
+// @route   GET api/posts
+// @desc    Get all post
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by Id
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (err) {
+    if (err.kind === 'ObjectId') {
+      return res
+        .status(404)
+        .json({ msg: 'Post not found' });
+    }
+
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
