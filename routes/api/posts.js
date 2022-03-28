af56870798bf80d69878400acb312d74cc15ff4d
trizzi <patrickakhamiogu@gmail.com>
2022-03-28 09:23:09 +0100

    Added delete post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -87,4 +87,37 @@ router.get('/:id', auth, async (req, res) => {
   }
 });
 
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ msg: 'Post not found' });
+    }
+
+    if (post.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post was removed' });
+  } catch (err) {
+    if (err.kind === 'ObjectId') {
+      return res
+        .status(404)
+        .json({ msg: 'Post not found' });
+    }
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
