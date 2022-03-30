855cb4686a2cf85637976d3bc06263a2c826eedc
trizzi <patrickakhamiogu@gmail.com>
2022-03-30 19:54:58 +0100

    Added Login and register route and also added use state hook to the register route

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,33 @@
+import { Fragment } from 'react';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './App.css';
-import { Fragment } from 'react';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+        </Routes>
+        <section className='container'>
+          <Routes>
+            <Route path='register' element={<Register />} />
+            <Route path='login' element={<Login />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </BrowserRouter>
   );
 };
 
--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
@@ -11,14 +12,12 @@ const Landing = () => {
             posts and get help from other developers
           </p>
           <div className='buttons'>
-            <a
-              href='register.html'
-              className='btn btn-primary'>
+            <Link to='register' className='btn btn-primary'>
               Sign up
-            </a>
-            <a href='login.html' className='btn btn-light'>
+            </Link>
+            <Link to='login' className='btn btn-light'>
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
