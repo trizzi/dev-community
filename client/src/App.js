86d76bc20c2efb9d4a3b101fa73e2b248a6651d4
trizzi <patrickakhamiogu@gmail.com>
2022-03-31 13:10:49 +0100

    Created redux store

--- client/src/App.js
+++ client/src/App.js
@@ -9,25 +9,33 @@ import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+// Redux
+import { Provider } from 'react-redux';
+import store from 'store';
 
 import './App.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Fragment>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Landing />} />
-        </Routes>
-        <section className='container'>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Fragment>
+          <Navbar />
           <Routes>
-            <Route path='register' element={<Register />} />
-            <Route path='login' element={<Login />} />
+            <Route path='/' element={<Landing />} />
           </Routes>
-        </section>
-      </Fragment>
-    </BrowserRouter>
+          <section className='container'>
+            <Routes>
+              <Route
+                path='register'
+                element={<Register />}
+              />
+              <Route path='login' element={<Login />} />
+            </Routes>
+          </section>
+        </Fragment>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
