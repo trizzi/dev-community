c996aed64a4512b50016a858202d28de400d991c
trizzi <patrickakhamiogu@gmail.com>
2022-03-31 14:54:11 +0100

    Added alerts and ist implimentation from the redux store

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,10 @@ import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layouts/Alert';
 // Redux
 import { Provider } from 'react-redux';
-import store from 'store';
+import store from './store';
 
 import './App.css';
 
@@ -25,6 +26,7 @@ const App = () => {
             <Route path='/' element={<Landing />} />
           </Routes>
           <section className='container'>
+            <Alert />
             <Routes>
               <Route
                 path='register'
